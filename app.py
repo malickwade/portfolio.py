af246f4a5ff3628fa3dd628de30745e6be38011d
malickwade <malickwa24@gmail.com>
2026-02-17 00:38:46 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,69 @@
+import streamlit as st
+
+with st.sidebar:
+    st.title("☎️INFORMATIONS")
+    st.image("ef.png")
+    st.text(" El Hadji Malick Wade")
+    st.text("Tel: 77 223 70 05")
+    st.text("email: wademalick02@gmail.com")
+
+
+    st.title("📘EDUCATIONS")
+    st.markdown("""
+
+    * BTS Geomatique: en cours 
+
+    * BAC L2: 2023-2024
+    
+    * BFEM: 2020-2024
+    
+    """)
+
+st.title("👮‍♂️Profile")   
+
+st.subheader(" Technicien supérieur en geomatique")
+
+
+
+st.title("📚Projet Academique")
+st.markdown("""
+
+* Réalisations des cartes thématiques sur ArcGis
+
+* Collecter des donnees au massalikul
+
+* Numérisation d'une base de données
+
+""")
+
+st.title("💀Experiences")
+st.markdown("""
+* Ouvrier a l'usine sips:2023-2024
+
+* Ouvrier a l'usine vitalait:2022-2023
+""")
+
+st.title("👨‍🦯‍➡️Projets Personnels")
+st.markdown("""
+* Réaliser une entreprise de geomaticien
+* Réalisation d'une etude SIG pour identifier les zones exposées aux indondations 
+""")
+
+
+
+
+
+    
+        
+
+
+
+
+
+
+
+
+
+            
+    
+            
